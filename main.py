d267faa1243fea379c64f4d2862cc95b8c9b9dd2
Pallav Pandey <pallav.pandey2011@gmail.com>
2023-06-12 17:27:02 +0530

    left over plots

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from environment import *
 from simulations import *
 
-M = 10						# number of players
+M = 2						# number of players
 n_exp = 5					# number of experiments to conduct
 deterministic_env = False	# reward observed is true mu or sampled
 randomization = False		# added randomization
@@ -10,13 +10,15 @@ skip_existing_sim = False	# skip if done already
 
 # time steps for each K
 dict_K_T = {
-	15 : 100000
+	3 : 100000, 
+	5 : 100000
 }
 
 # key: number of arms (K), value: true mu values
 dict_true_mu = {
 	# 3 : [[0.1, 0.5, 0.9]], 
-    15: [np.linspace(0.1, 0.2, 15), np.linspace(0.8, 0.9, 15), np.linspace(0.01, 0.99, 15)]
+    3: [np.linspace(0.1, 0.2, 15), np.linspace(0.8, 0.9, 15), np.linspace(0.01, 0.99, 15)], 
+    5: [np.linspace(0.1, 0.2, 15), np.linspace(0.8, 0.9, 15), np.linspace(0.01, 0.99, 15)]
 }
 
 # list of all combinations of environment
